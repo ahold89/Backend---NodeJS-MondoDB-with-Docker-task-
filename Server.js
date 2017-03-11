3ccffe90d890469197672c1f42e744bf7d3f9b6d
asherh <asherh@liveperson.com>
2017-03-11 19:18:33 +0200

    Added connection pooling for mongoDB

--- Server.js
+++ Server.js
@@ -10,6 +10,7 @@ var costCollection = 'costumers';
 var assert = require('assert');
 var mongoClient = mongo.MongoClient;
 app.use(bodyParser.json());
+var mongodb;
 
 // Build a service that manages customers.
 // The service needs to have the following functionality:
@@ -40,46 +41,34 @@ var validateCostumer = schema({
   credit_card_tokens: Array //This is validated in a different service
 })
 
+mongoClient.connect(url, {
+    poolSize: 5
+  },
+  function(err, db) {
+    if(err) throw err;
+    mongodb = db;
+  }
+);
+
 app.route('/costumers')
   .get(function(req, res){ //fetchById
-    var id = req.query["id"];
+    var idQuery = req.query["id"];
     var resultArray = [];
     var validateIdRegex = new RegExp("^[0-9]{1,9}$");
-    if (!validateIdRegex.test(id)) {
+    if (!validateIdRegex.test(idQuery)) {
       res.status(400).send("The supplied ID isn't valid");
       return;
     }else{
-      mongoClient.connect(url, function(err, db){
-        // assert.equal(null, err);
+      mongodb.collection(costCollection).find({id:idQuery}).limit(1).next(function(err, doc) {
         if (err != null){
           console.log(err.message);
-          db.close();
           res.status(500).send("An error occurred, please try again");
           return;
         }
-        var cursor = db.collection(costCollection).find({"id":id});
-        cursor.forEach(function(doc,err) {
-                        //  assert.equal(null, err)
-                        if (err != null){
-                            console.log(err.message);
-                            db.close();
-                            res.status(500).send("An error occurred, please try again");
-                            return;
-                         }
-                         resultArray.push(doc);
-                       },
-                       function() {
-                         db.close();
-                         if (resultArray.length > 0) {
-                           console.log("Fetched costumer succesfully")
-                           res.status(200).send(resultArray);
-                         } else{
-                           console.log("No costumer exists with id: ", id);
-                           res.status(400).send("No costumer exists with id: ''" + id + "'");
-                         }
-                       }
-                      )}
-      )
+        console.log("This is doc:", doc);
+        res.send(doc);
+        return;
+      })
     }
   })
   .patch(function(req, res){ //updateCostumer
@@ -92,31 +81,26 @@ app.route('/costumers')
     }
 
     //Update costumer:
-    mongoClient.connect(url, function(err, db) {
-      db.collection(costCollection).findAndModify(
-        {id: req.body.id},     // query
-        [],               // represents a sort order if multiple matches
-        { $set: {name:req.body.name, email:req.body.email, address:req.body.address, credit_card_tokens:req.body.credit_card_tokens}},   // update statement
-        { new: true },    // options - new to return the modified document
-        function(err,doc) {
-          if (err == null) {
-            if (doc.lastErrorObject.updatedExisting) {
-              db.close();
-              console.log(doc.value);
-              res.status(200).send("Managed to update costumer with id: '" + req.body.id + "'");
-            }else{
-              db.close();
-              console.log("Client tried to update a non-existing costumer/Change the ID");
-              res.status(403).send("You cannot change your ID/There is no pre-existing account with this ID, please create account");
-            }
-          }else {
-            console.log("An error occurred: ", err.message);
-            db.close();
-            res.status(500).send(err.message);
+    mongodb.collection(costCollection).findAndModify(
+      {id: req.body.id},     // query
+      [],               // represents a sort order if multiple matches
+      { $set: {name:req.body.name, email:req.body.email, address:req.body.address, credit_card_tokens:req.body.credit_card_tokens}},   // update statement
+      { new: true },    // options - new to return the modified document
+      function(err,doc) {
+        if (err == null) {
+          if (doc.lastErrorObject.updatedExisting) {
+            console.log(doc.value);
+            res.status(200).send("Managed to update costumer with id: '" + req.body.id + "'");
+          }else{
+            console.log("Client tried to update a non-existing costumer/Change the ID");
+            res.status(403).send("You cannot change your ID/There is no pre-existing account with this ID, please create account");
           }
+        }else {
+          console.log("An error occurred: ", err.message);
+          res.status(500).send(err.message);
         }
-      );
-    });
+      }
+    )
   })
   .delete(function(req, res) { //deleteCostumer
       var queryId = req.query["id"];
@@ -125,27 +109,22 @@ app.route('/costumers')
         res.status(404).send("The supplied ID isn't valid");
         return;
       }else{
-        mongoClient.connect(url, function(err, db) {
-          db.collection(costCollection).findAndRemove({id: queryId}, [], function(err, doc) {
+        mongodb.collection(costCollection).findAndRemove({id: queryId}, [], function(err, doc) {
             if (err == null){
               if (doc.value != null){
                 console.log("Costumer deleted:'" + queryId + "'");
                 console.log(doc);
-                db.close();
                 res.status(200).send("Costumer deleted:'" + queryId + "'"); //not using 204 in order to add response body
               }else {
                 console.log("There is no costumer:'" + queryId + "'");
                 console.log(doc);
-                db.close();
                 res.status(404).send("There is no costumer:'" + queryId + "'");
               }
             }else{
               console.log(err.message);
-              db.close();
               res.status(500).send("An error ocurred, please try again");
             }
           });
-        });
       }
 
   })
@@ -161,46 +140,31 @@ app.route('/costumers')
 
     //Insert to mongoDB
     var insertDocument = function(db, callback) {
-     db.collection(costCollection)
-       .insertOne(req.body, function(err, result) {
-                    if (err == null) { //no errors
-                        callback();
-                    }else {
-                      if (err.code == 11000) { //Uniqueness is on the line
-                        console.log(err.message);
-                        db.close();
-                        res.status(400).send("There is already an account with the provided id");
-                        return;
-                      }else { //Some other error
-                        console.log("Something went wrong");
-                        db.close();
-                        res.status(500).send("Something went wrong, please try again");
-                        return;
+      db.collection(costCollection)
+        .insertOne(req.body, function(err, result) {
+                      if (err == null) { //no errors
+                          callback();
+                      }else {
+                        if (err.code == 11000) { //Uniqueness is on the line
+                          console.log(err.message);
+                          res.status(400).send("There is already an account with the provided id");
+                          return;
+                        }else { //Some other error
+                          console.log("Something went wrong");
+                          res.status(500).send("Something went wrong, please try again");
+                          return;
+                        }
                       }
-                    }
-                  });
+                    });
     };
 
-    mongoClient.connect(url, function(err, db) {
-      if (err != null){
-        console.log(err.message)
-      }
-      // assert.equal(null, err);
-      if (err != null){
-        console.log(err.message);
-        db.close();
-        res.status(500).send("An error occurred, please try again");
-        return;
-      }
-
-      //ID is unique - no duplicates allowed:
-      db.collection(costCollection).ensureIndex({id:1}, {unique:true}, function(err, indexName){
-        insertDocument(db, function() {
-                         db.close();
-                         console.log("Costumer was created");
-                         res.status(201).send("Thank you for creating an account " + req.body["name"]);
-                       });
-      });
+    //ID is unique - no duplicates allowed:
+    mongodb.collection(costCollection).ensureIndex({id:1}, {unique:true}, function(err, indexName){
+      insertDocument(mongodb, function() {
+                        
+                        console.log("Costumer was created");
+                        res.status(201).send("Thank you for creating an account " + req.body["name"]);
+                      });
     });
   });
 
